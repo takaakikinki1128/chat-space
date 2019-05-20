4d70d01cfa6b795aa0c7f15aa4aea77e21a6512a
出口貴章 <chukouguizhang@deguchitakaakinoMacBook-Air.local>
2019-05-20 14:58:00 +0900

    すいませんプルリクエストこまめに出すの忘れてました

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,36 @@
+class GroupsController < ApplicationController
+  def index
+  end
+  def new
+    @group = Group.new
+    @group.users<< current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, nothce: 'グループが作成されました'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), nothce: 'グループが編集されました'
+    else
+      render :edit
+    end
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name,{:user_ids => []})
+  end
+end
+
