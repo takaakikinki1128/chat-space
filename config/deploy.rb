3e76d4ee4d3d9d74e6a0f3eedb366e36bc832f4f
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-05-29 13:40:28 +0900

    画像のデプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,16 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 set :application, "chat-space"
 set :repo_url, "git@github.com:takaakikinki1128/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, %w{ config/secrets.yml }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -21,6 +26,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
+
 end
 
 # Default branch is :master
